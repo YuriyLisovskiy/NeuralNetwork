06a3b02f404e30ae27598f46397ffa3acbec2853
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-01-23 00:39:36 +0200

    updated network train() method

--- neural_network/src/network.py
+++ neural_network/src/network.py
@@ -16,6 +16,10 @@ class NeuralNetwork(object):
 	def sigmoid(x):
 		return 1 / (1 + np.exp(-x))
 
+	@staticmethod
+	def derivative_of_sigmoid(x):
+		return x * (1 - x)
+
 	def predict(self, inputs):
 		res = inputs
 		for weight in self.weights:
@@ -32,15 +36,13 @@ class NeuralNetwork(object):
 			res = self.sigmoid_mapper(res)
 			outputs.append(res)
 		actual_predict = res[0]
-		index = len(self.weights) - 1
+		weights_index = len(self.weights) - 1
 		j = len(outputs) - 2
 		error_layer = np.array([actual_predict - expected_predict])
-		gradient_layer = actual_predict * (1 - actual_predict)
-		weights_delta = error_layer * gradient_layer
-		self.weights[index] -= (np.dot(weights_delta, outputs[j].reshape(1, len(outputs[j])))) * self.learning_rate
-		for i in range(index, 0, -1):
+		weights_delta = error_layer * self.derivative_of_sigmoid(actual_predict)
+		self.weights[weights_index] -= (np.dot(weights_delta, outputs[j].reshape(1, len(outputs[j])))) * self.learning_rate
+		for i in range(weights_index, 0, -1):
 			error_layer = weights_delta * self.weights[i]
-			gradient_layer = outputs[j] * (1 - outputs[j])
-			weights_delta = error_layer * gradient_layer
+			weights_delta = error_layer * self.derivative_of_sigmoid(outputs[j])
 			self.weights[i - 1] -= np.dot(outputs[j - 1].reshape(len(outputs[j - 1]), 1), weights_delta).T * self.learning_rate
 			j -= 1
