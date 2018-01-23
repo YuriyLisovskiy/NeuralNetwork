bugfix and added raising errors for incorrect values in NeuralNetwork constructor

neural_network/src/learning/training.py
@@ -4,7 +4,7 @@ from neural_network.config.config import EPOCHS
 
 
 def mse(y_1, y_2):
-	return np.mean((y_1 - y_2)**2)
+	return np.mean((y_1 - y_2) ** 2)
 
 
 def training(neural_net, training_data):
@@ -14,7 +14,7 @@ def training(neural_net, training_data):
 		inputs = []
 		correct_predictions = []
 		for input_stat, correct_predict in training_data:
-			neural_net.back_prop_train(np.array(input_stat), correct_predict)
+			neural_net.back_prop_train(np.array(input_stat), np.array(correct_predict))
 			inputs.append(np.array(input_stat))
 			correct_predictions.append(np.array(correct_predict))
 

neural_network/src/network/net.py
@@ -6,10 +6,16 @@ class NeuralNetwork(object):
 	weights = []
 
 	def __init__(self, layers, learning_rate=0.1):
+		if layers[len(layers) - 1] != 1:
+			raise ValueError('Invalid last layer: the last layer must have only one neuron')
 		for i in range(len(layers) - 1):
+			correct_number = 2 ** layers[i]
+			if layers[i + 1] > correct_number:
+				raise ValueError('An amount of neurons in layer #{} is {}, {} neuron is redundant'.format(
+					i + 2, layers[i + 1], layers[i + 1] - correct_number))
 			self.weights.append(np.random.normal(0.0, 2 ** -0.5, (layers[i + 1], layers[i])))
 		self.sigmoid_mapper = np.vectorize(self.sigmoid)
-		self.learning_rate = [learning_rate]
+		self.learning_rate = np.array([learning_rate])
 
 	@staticmethod
 	def sigmoid(x):
@@ -36,15 +42,18 @@ class NeuralNetwork(object):
 			res = self.sigmoid_mapper(res)
 			outputs.append(res)
 
-		actual_predict = res[0]
+		actual_predict = res
 		weights_index = len(self.weights) - 1
 		outputs_index = len(outputs) - 2
 		error_layer = np.array([actual_predict - expected_predict])
 		weights_delta = error_layer * self.derivative_of_sigmoid(actual_predict)
-		self.weights[weights_index] -= (np.dot(weights_delta, outputs[outputs_index].reshape(1, len(outputs[outputs_index])))) * self.learning_rate
+		self.weights[weights_index] -= (np.dot(weights_delta.reshape(weights_delta.shape[1], 1),
+												outputs[outputs_index].reshape(1, len(
+													outputs[outputs_index])))) * self.learning_rate
 
 		for i, j in zip(range(weights_index, 0, -1), range(outputs_index, 0, -1)):
-			error_layer = weights_delta * self.weights[i]
+			error_layer = np.dot(weights_delta, self.weights[i])
 			weights_delta = error_layer * self.derivative_of_sigmoid(outputs[j])
-			self.weights[i - 1] -= np.dot(outputs[j - 1].reshape(len(outputs[j - 1]), 1), weights_delta).T * self.learning_rate
+			self.weights[i - 1] -= np.dot(outputs[j - 1].reshape(len(outputs[j - 1]), 1),
+											weights_delta).T * self.learning_rate
 			j -= 1

neural_network/tests/unittest/test_case_1.py
@@ -4,7 +4,7 @@ import numpy as np
 def test_with_bool(neural_net, training_data):
 	for input_stat, correct_predict in training_data:
 		print(" For input: {} the prediction is: {}, expected: {}".format(str(input_stat),
-			str(neural_net.predict(np.array(input_stat)) > 0.5), str(correct_predict == 1)))
+			str(neural_net.predict(np.array(input_stat)) > 0.5), str(np.array(correct_predict) == 1)))
 
 
 def test_with_numbers(neural_net, training_data):