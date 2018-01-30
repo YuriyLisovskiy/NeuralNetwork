f17d9b43e73347c3a6cb39a2066c8bdbc08e835a
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-01-30 14:32:06 +0200

    small fix

--- neural_network/learning/training.py
+++ neural_network/learning/training.py
@@ -1,15 +1,14 @@
 import sys
 import numpy as np
-from neural_network.config.config import EPOCHS
 
 
 def mse(y_1, y_2):
 	return np.mean((y_1 - y_2) ** 2)
 
 
-def training(neural_net, training_data):
+def training(neural_net, training_data, epochs):
 	train_loss = None
-	for e in range(EPOCHS):
+	for e in range(epochs):
 		inputs = []
 		correct_predictions = []
 		for input_stat, correct_predict in training_data:
@@ -19,6 +18,6 @@ def training(neural_net, training_data):
 
 		train_loss = mse(neural_net.predict(np.array(inputs).T), np.array(correct_predictions))
 		sys.stdout.write(
-			"\r Progress: {}%, Training loss: {}".format(str(100 * e / float(EPOCHS))[:4], str(train_loss)[:5]))
+			"\r Progress: {}%, Training loss: {}".format(str(100 * e / float(epochs))[:4], str(train_loss)[:5]))
 	sys.stdout.write(
 		"\r Progress: 100%, Training loss: {}\nDone.\n\n".format(str(train_loss)[:5]))
--- tests/unittest/test_output.py
+++ tests/unittest/test_output.py
@@ -1,7 +1,7 @@
 import numpy as np
 import unittest
 from neural_network.network import net
-from neural_network.config.config import LEARNING_RATE, LAYERS
+from neural_network.config.config import LEARNING_RATE, LAYERS, EPOCHS
 from neural_network.learning.training import training
 
 """
@@ -39,14 +39,14 @@ class TestOutput(unittest.TestCase):
 		([1, 1, 1], 1)
 	]
 	neural_net = net.NeuralNetwork(layers=LAYERS, learning_rate=LEARNING_RATE)
-	training(neural_net=neural_net, training_data=training_data)
+	training(neural_net=neural_net, training_data=training_data, epochs=EPOCHS)
 
 	def make_predict(self, input_data):
 		return self.neural_net.predict(np.array(input_data))
 
 	def test_bool_result(self):
 		for input_data, correct_predict in self.training_data:
-			self.assertEqual(self.make_predict(input_data) > 0.5, np.array(correct_predict) == 1)
+			self.assertEqual(self.make_predict(input_data) >= 0.5, np.array(correct_predict) == 1)
 
 	def test_numbers_result(self):
 		pass
