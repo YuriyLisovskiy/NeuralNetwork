c385599454ad217dde45b9d5a4f21a8a98de6111
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-01-31 22:35:43 +0200

    updated tests

--- tests/unittest/test_exceptions.py
+++ tests/unittest/test_exceptions.py
@@ -22,8 +22,28 @@ class TestExceptions(unittest.TestCase):
 				'output_layer': OUTPUT_LAYER
 			}
 			net.NeuralNetwork(**params)
+			
+	def test_input_layer_exception(self):
+		with self.assertRaises(ValueError):
+			params = {
+				'input_layer': INPUT_LAYER,
+				'hidden_layers': HIDDEN_LAYERS,
+				'output_layer': [1, 2]
+			}
+			net.NeuralNetwork(**params)
+	
+	def test_output_layer_exception(self):
+		with self.assertRaises(ValueError):
+			params = {
+				'input_layer': [1, 1],
+				'hidden_layers': HIDDEN_LAYERS,
+				'output_layer': [1]
+			}
+			net.NeuralNetwork(**params)
 
 
 def run(suite):
 	suite.addTest(TestExceptions('test_last_layer_exception'))
 	suite.addTest(TestExceptions('test_redundant_layers_exception'))
+	suite.addTest(TestExceptions('test_input_layer_exception'))
+	suite.addTest(TestExceptions('test_output_layer_exception'))
